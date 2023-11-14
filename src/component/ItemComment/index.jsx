1e2038091c63560dbd53955f32acf37e27925794
King <>
2023-11-15 00:52:21 +0700

    feat: add lazy load & remove comment

--- src/component/ItemComment/index.jsx
+++ src/component/ItemComment/index.jsx
@@ -4,7 +4,7 @@ export default function ItemComment({ item }) {
     return (
         <div key={item.id} className='mt-5 border-b-2 pb-10'>
             <div className='flex gap-4 items-center'>
-                <img src="https://0.gravatar.com/avatar/?s=96&d=identicon&r=G" alt='img user' className='rounded-full w-12 h-12 border' />
+                <img loading='lazy' src="https://0.gravatar.com/avatar/?s=96&d=identicon&r=G" alt='img user' className='rounded-full w-12 h-12 border' />
                 <div>
                     <p className='text-lg font-bold'>{item.name}</p>
                     <p className='text-sm font-light'>{item.email}</p>
--- src/component/PostPopular/item.jsx
+++ src/component/PostPopular/item.jsx
@@ -2,33 +2,23 @@ import React from 'react'
 
 export default function Item({ item, onClick = () => { }, className = '' }) {
     return (
-        // <div onClick={() => onClick()} className='flex gap-2 mt-4 rounded-xl p-3 shadow-xl items-center cursor-pointer'>
-        //     <img className='w-[132px] max-h-[70px] rounded-xl' alt={item?.title} src='https://via.placeholder.com/600/cde4c1' />
-        //     <div className={`max-w-[420px] ${className}`}>
-        //         <h1 className='text-sm font-bold mb-4 truncate'>
-        //             {item?.title}
-        //         </h1>
-        //         <p className='text-xs truncate'>{item?.body}</p>
-        //     </div>
-
-        // </div>
         <div
-        onClick={() => onClick()}
-        className="flex flex-col md:flex-row gap-2 md:gap-4 lg:gap-6 mt-4 rounded-xl p-3 shadow-xl items-center cursor-pointer"
-      >
-        <img
-          className="w-full md:w-[132px] max-h-[70px] md:max-h-[100px] object-cover mb-2 md:mb-0"
-        // className="w-full object-cover mb-2 md:mb-0"
-          alt={item?.title}
-          src="https://via.placeholder.com/600/cde4c1"
-        />
-        <div className={`flex-1 max-w-full md:max-w-[420px] overflow-hidden ${className}`}>
-          <h1 className="text-sm font-bold mb-2 md:mb-0 truncate">
-            {item?.title}
-          </h1>
-          <p className="text-xs truncate">{item?.body}</p>
+            onClick={() => onClick()}
+            className="flex flex-col md:flex-row gap-2 md:gap-4 lg:gap-6 mt-4 rounded-xl p-3 shadow-xl items-center cursor-pointer"
+        >
+            <img
+                loading='lazy'
+                className="w-full md:w-[132px] max-h-[70px] md:max-h-[100px] object-cover mb-2 md:mb-0"
+                alt={item?.title}
+                src="https://via.placeholder.com/600/cde4c1"
+            />
+            <div className={`flex-1 max-w-full md:max-w-[420px] overflow-hidden ${className}`}>
+                <h1 className="text-sm font-bold mb-2 md:mb-0 truncate">
+                    {item?.title}
+                </h1>
+                <p className="text-xs truncate">{item?.body}</p>
+            </div>
         </div>
-      </div>
-      
+
     )
 }
